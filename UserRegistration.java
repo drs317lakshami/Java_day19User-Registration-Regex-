957948da854086f16810a20276463347082fb1b0
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-05 00:51:34 +0530

    Validate first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,97 +10,25 @@ public class UserRegistration {
 		System.out.println("Regular Expression program to validate User Registration!!!");
 		UserRegistration userEntry = new UserRegistration();
 
-		userEntry.testMails();
+		System.out.println("Enter First Name : ");
+		userEntry.checkFirstName(userEntry.getUserInput());
 	}
 
-	public void validateEntries() {
-		System.out.println("Enter First Name :");
-		validateName(getUserInput());
-
-		System.out.println("Enter Last Name :");
-		validateName(getUserInput());
-
-		System.out.println("\nEnter Email Id : ");
-		validateEmail(getUserInput());
-
-		System.out.println("\nEnter Mobile Number : ");
-		validateMobileNumber(getUserInput());
-		
-		System.out.println("\nEnter Password : ");
-		validatePassword(getUserInput());
-
-	}
-
-	public String getUserInput() {
+	private String getUserInput() {
 		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
-	}
-	
-	public void testMails() {
-		validateEmail("abc@yahoo.com");
-		validateEmail("abc");
-		validateEmail("abc-100@yahoo.com");
-		validateEmail("abc@.com.my");
-		validateEmail("abc.100@yahoo.com");
-		validateEmail("abc123@gmail.a");
-		validateEmail("abc111@abc.com");
-		validateEmail("abc123@.com");
-		validateEmail("abc-100@abc.net");
-		validateEmail("abc123@.com.com");
-		validateEmail("abc.100@abc.com.au");
-		validateEmail(".abc@abc.com");
-		validateEmail("abc@1.com");
-		validateEmail("abc()*@gmail.com");
-		validateEmail("abc@gmail.com.com");
-		validateEmail("abc@%*.com");
-		validateEmail("abc+100@gmail.com");
-		validateEmail("abc..2002@gmail.com");
-		validateEmail("abc.@gmail.com");
-		validateEmail("abc@abc@gmail.com");
-		validateEmail("abc@gmail.com.1a");
-		validateEmail("abc@gmail.com.aa.au");
-	}
-	
-	private void validatePassword(String input) {
-		String password = "^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$";
-		
-		System.out.println("\nValidating Password...");
-		match(password, input);
+		return sc.next();
 	}
 
-	public void validateMobileNumber(String input) {
+	private void checkFirstName(String input) {
 
-		String mobile = "^[0-9]{2}\\s[0-9]{10}$";
+		String fName = "^[A-Z][a-zA-Z]{3,}$";
 
-		System.out.println("\nValidating Mobile Number...");
-		match(mobile, input);
-	}
-
-	public void validateEmail(String input) {
-
-		String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
-
-		System.out.println("\nValidating Email...");
-		match(email, input);
-	}
-
-	public void validateName(String input) {
-
-		String name = "^[A-Z][a-zA-Z]{3,}$";
-
-		Pattern pattern = Pattern.compile(name);
+		Pattern pattern = Pattern.compile(fName);
 		Matcher match = pattern.matcher(input);
 
-		System.out.println("Validating Name...");
+		System.out.println("Validating First Name...");
 		printResult(match.matches(), input);
 
-	}
-
-	public void match(String regex, String input) {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher match = pattern.matcher(input);
-
-		printResult(match.matches(), input);
 	}
 
 	public void printResult(boolean isOk, String input) {
